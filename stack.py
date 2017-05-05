9af3d3dfde352878e0672fe49d949f59b52b3a72
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2017-05-05 19:00:43 +0900

    Add example_multi_input_and_multi_output

--- stack.py
+++ stack.py
@@ -21,7 +21,8 @@ Examples
 
 
 from keras.models import Model
-from keras.layers import Input, Dense, Activation, Conv2D, Add, Multiply
+from keras.layers import Input, Dense, Activation, Conv2D, Embedding, LSTM
+from keras.layers import Add, Multiply, Concatenate
 from keras.utils import plot_model
 
 def example_0():
@@ -85,3 +86,23 @@ def example_residual_connection():
     model = Model(input, output)
     plot_model(model, to_file='example_residual_connection.png', show_shapes=True, show_layer_names=False)
 
+def example_multi_input_and_multi_output():
+    main_input = Input(shape=(100,), dtype='int32', name='main_input')
+    auxiliary_input = Input(shape=(5,), name='aux_input')
+    outputs = stack(
+        main_input,
+        Embedding(output_dim=512, input_dim=10000, input_length=100),
+        LSTM(32),
+        [Dense(1, activation='sigmoid', name='aux_output'),
+         ([auxiliary_input, lambda x: x],
+          Concatenate(),
+          Dense(64, activation='relu'),
+          Dense(64, activation='relu'),
+          Dense(64, activation='relu'),
+          Dense(1, activation='sigmoid', name='main_output')
+         )
+        ]
+    )
+    model = Model([main_input, auxiliary_input], outputs)
+    plot_model(model, to_file='example_multi_input_and_multi_output.png')
+          
